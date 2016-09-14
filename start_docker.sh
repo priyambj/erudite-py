478850337db12d114b2906502fb8d6eb50ffa792
Florian Geigl <florian.geigl@tugraz.at>
2016-09-13 21:41:58 -0700

    Update start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run -i -t -v $PWD:erudite/ -w=erudite/ --rm -p 8888:8888 floriangeigl/erudite
+if [ $# -eq 0 ]; then
+  jport=8888
+else
+  jport=$1
+docker run -i -t -v $PWD:erudite/ -w=erudite/ --rm -p $jport:8888 floriangeigl/erudite
