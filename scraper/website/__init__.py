9b45575d31beccd5b7a60360dfd284ab25f35a63
priyambj <priyambj@usc.edu>
2016-10-31 11:42:12 -0700

    Update __init__.py

--- scraper/website/__init__.py
+++ scraper/website/__init__.py
@@ -1,2 +1,5 @@
 from website_interface import WebsiteInterface
 from coursera import Coursera
+from bigdatauniversity import BigDataUniversity
+from ebi import EBI
+from videolectures import VideoLectures
